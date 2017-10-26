6b977e19369bd5a30fbaaa2fc2016f646e76dab3
Brian <karabinchak.brian@gmail.com>
2017-10-26 13:45:32 -0400

    Basic tweeting functionality using tweet ID's

--- index.js
+++ index.js
@@ -79,6 +79,7 @@ bot.get("friends/list", {screen_name: "bkchak"},
 
 //To get the relation between us and a specific user
 //useful for following people who don't have the following connection
+/*
 bot.get("friendships/lookup", {screen_name:"LeeCamp"},
 	function(err, data, response) {
 		if(err){
@@ -87,9 +88,11 @@ bot.get("friendships/lookup", {screen_name:"LeeCamp"},
 			console.log(data);
 		}
 	});
+*/
 
 //Never DM someone who's not following your bot
 //How to DM someone
+/*
 bot.post("direct_messages/new", {screen_name:""},
 	function(err, data, response){
 		if(err){
@@ -98,3 +101,90 @@ bot.post("direct_messages/new", {screen_name:""},
 			console.log(data);
 		}
 	});
+*/
+
+//Get home timeline
+function getBotTimeline(){
+	bot.get("statuses/home_timeline", {count: 1},
+		function(err, data, response){
+			if(err){
+				console.log(err);
+			} else {
+				data.forEach(function(d){
+					console.log(d.text);
+					console.log(d.user.screen_name);
+					console.log(d.id_str);
+					console.log("\n");
+				})
+			}
+		});
+}
+
+// getBotTimeline();
+
+
+//These all use d.id_str;
+//How to rewteet
+function retweetByID(id){
+bot.post("statuses/retweet/:id", {id: id},
+	function(err, data, response){
+		if(err){
+			console.log(err);
+		} else {
+			console.log(data.text + " was retweeted");
+		}
+	});	
+}
+
+
+//to like something
+function likeTweetById(id){
+	var id_str = "" + id;
+	bot.post("favorites/create", {id: id_str},
+		function(err, data,response){
+			if(err){
+				console.log(err);
+			} else {
+				console.log(data.text + " was liked.");
+			}
+		});	
+}
+
+function unlikeTweetById(id) {
+	var id_str = "" + id;
+	bot.post("favorites/destroy", {id: id_str},
+		function(err, data,response){
+			if(err){
+				console.log(err);
+			} else {
+				console.log(data.text + " was unliked.");
+			}
+		});	
+};
+
+//To reply to a tweet
+function replyToTweetById(replyTo, reply, id){
+	var replyMessage = "@" + replyTo + " " + reply;
+	bot.post("statuses/update", {status: replyMessage,
+		in_reply_to_status_id: id},
+		function(err, data, response) {
+			if(err){
+				console.log(err);
+			} else {
+				console.log(data);
+			}
+		}
+	)
+};
+
+//to delete a tweet
+function deleteTweetById(id) {
+	bot.post("statuses/destroy/:id", {id: id},
+		function(err, data, response){
+			if(err){
+				console.log(err);
+			} else {
+				console.log(data.text + " was deleted.");
+			}
+		});
+}
